8b821aa1f8fd5d957a9f560fead874777c718605
RSchumacher13 <48203929+RSchumacher13@users.noreply.github.com>
2019-07-12 15:03:14 +0200

    Adds reference fetching feature + tests (#8)
    * Adds reference fetching feature + tests
    
    * fix PSR syntax compliacne

---
+++ src/Entity/Reference.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Continuous\Sdk\Entity;
+
+/**
+ * Class Reference
+ * @package Continuous\Sdk\Entity
+ */
+class Reference extends EntityAbstract
+{
+    public function hydrate(array $attributes)
+    {
+        $this->id = $attributes['referenceId'];
+        $this->attributes = $attributes;
+    }
+}
