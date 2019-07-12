862da12dab7dd54952c8c53a7aba347f0b033e31
Raphael Schumacher <raphael.schumacher@continuous.team>
2019-07-12 15:49:17 +0200

    add missing annotation in order to make magic method usable by Guzzle

--- src/Client.php
+++ src/Client.php
@@ -27,6 +27,7 @@ use GuzzleHttp\Command\Guzzle\GuzzleClient;
  * @method    array getPipelines(array $args = array())
  * @method    array putPipeline(array $args = array())
  * @method    array getBuilds(array $args = array())
+ * @method    array getReferences(array $args = array())
  * @method    \Continuous\Sdk\Entity\Build startBuild(array $args = array())
  * @method    \Continuous\Sdk\Entity\Build getBuild(array $args = array())
  * @method    \Continuous\Sdk\Entity\WebHooks resetWebHooks(array $args = array())
@@ -59,31 +60,31 @@ class Client extends GuzzleClient
             throw new \InvalidArgumentException("destination argument is not provided");
         }
         $destination = $args['destination'];
-        
+
         unset($args['destination']);
         /** @var Package $package */
         $package = $this->getPackage($args);
 
         $url = $package->get('url');
         $path = $destination . '/' . pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_BASENAME);
-        
+
         $fp = fopen($path, 'w+');
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_FILE, $fp);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $result = curl_exec($ch);
-        
+
         if (!$result) {
             $exception = new Exception("The download failed with the following error: " . curl_error($ch));
         }
-        
+
         curl_close($ch);
         fclose($fp);
-        
+
         if (isset($exception)) {
             throw $exception;
         }
-        
+
         return ['path' => $path];
     }
 }
